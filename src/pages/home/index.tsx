e2f7462472f6ce9ed18fdc54d9325382cb13d6f0
giovani <giovaniocan@gmail.com>
2023-06-02 13:36:34 -0300

    fix the navigator

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,8 +2,9 @@ import { Navigator } from '@/components/Navigator'
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen p-5   ">
+    <div className="h-screen w-screen p-5 flex  ">
       <Navigator />
+      ola
     </div>
   )
 }
