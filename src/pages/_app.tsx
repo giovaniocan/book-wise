9b3c6c3fac0d69ea6466676e3210546642261997
giovani <giovaniocan@gmail.com>
2023-06-12 16:54:25 -0300

    it's possible to connect with google and github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable camelcase */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 import { Nunito_Sans } from 'next/font/google'
 
 const nunito = Nunito_Sans({ subsets: ['latin'] })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <main className={nunito.className}>
-      <Component {...pageProps} />
-    </main>
+    <SessionProvider session={session}>
+      <main className={nunito.className}>
+        <Component {...pageProps} />
+      </main>
+    </SessionProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
+import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 export default function Home() {
   const router = useRouter()
 
-  function LogInWithGoogle() {
-    window.alert('login não ta pronto')
+  function LogInWithGitHub() {
+    signIn('github')
   }
 
-  function LogInWithGitHub() {
-    window.alert('login não ta pronto')
+  function LogInWithGoogle() {
+    signIn('google')
+    router.push('/home')
   }
 
   function LogInAsvisitor() {
     router.push('/home')
   }
+
+  const session = useSession()
 
   return (
     <div className="h-screen w-screen flex p-5 items-center ">
@@ -68,6 +72,7 @@ export default function Home() {
                 Entrar como visitante
               </span>
             </button>
+            {JSON.stringify(session.data)}
           </div>
         </div>
       </div>
